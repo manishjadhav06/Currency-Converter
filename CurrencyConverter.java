44e2bb55955dd01f6a7d9d9fdaad81482d914508
manishjadhav06 <manishjadhav.occ@gmail.com>
2025-01-30 13:43:55 +0530

    conversion is working correctly

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -1,12 +1,16 @@
 import java.net.URL;
-import java.net.MalformedURLException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Scanner;
 import java.util.HashMap;
+import org.json.JSONObject;
 
 public class CurrencyConverter {
-    public static void main(String[] args) throws MalformedURLException, URISyntaxException{
+    public static void main(String[] args) throws URISyntaxException, IOException{
         HashMap<Integer, String> currencyCodes = new HashMap<Integer,String>();
 
         // adding currency codes
@@ -37,12 +41,40 @@ public class CurrencyConverter {
         System.out.println("Thanks for using the converter!");
     }
 
-    private static void sendHttpGETRequest(String fromCode, String toCode, double amount) throws MalformedURLException, URISyntaxException{
-        // String GET_URL ="https://v6.exchangerate-api.com/v6/936c8ca3cf6e00580fe6a4bc/pair/"+fromCode+"/"+toCode+"/"+amount;
-        // URL url= new URL("https://v6.exchangerate-api.com/v6/936c8ca3cf6e00580fe6a4bc/pair/"+fromCode+"/"+toCode+"/"+amount);
-        // https://v6.exchangerate-api.com/v6/936c8ca3cf6e00580fe6a4bc/pair/INR/USD/100
+    private static void sendHttpGETRequest(String fromCode, String toCode, double amount) throws URISyntaxException, IOException{
 
         URI uri = new URI("https", "v6.exchangerate-api.com", "/v6/936c8ca3cf6e00580fe6a4bc/pair/" + fromCode + "/" + toCode + "/" + amount, null);
         URL url = uri.toURL();
+
+        //https://v6.exchangerate-api.com/v6/936c8ca3cf6e00580fe6a4bc/pair/USD/INR/1
+
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.setRequestMethod("GET");
+        int responseCode = httpURLConnection.getResponseCode();
+
+        if(responseCode==HttpURLConnection.HTTP_OK){
+            BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while((inputLine=in.readLine())!=null){
+                response.append(inputLine);
+            }in.close();
+
+            JSONObject obj = new JSONObject(response.toString());
+
+            System.out.println("JSON Response: " + response.toString());
+
+            if (obj.has("conversion_rate")) {
+                Double exchangeRate = obj.getDouble("conversion_rate");
+                System.out.println("Conversion rate: " + exchangeRate);
+                System.out.println(amount + fromCode + " = " + amount / exchangeRate + toCode);
+            } else {
+                System.out.println("Conversion rate is not available.");
+            }
+        }else{
+            System.out.println("Didn't work.");
+        }
+        
     }
 }
